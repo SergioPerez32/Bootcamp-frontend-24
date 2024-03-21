031c4b2bbbfc9f57803854a8b529975f0d559ecb
SergioPerez32 <sergiorlando10@hotmail.com>
2024-03-21 17:53:40 -0400

    c

--- js-challenge-4.js
+++ js-challenge-4.js
@@ -1,4 +1,4 @@
 console.log(1);//First to be executed
 setTimeout(function(){console.log(2);},1000);//enters queue first=>Fourth to be executed
-setTimeout(function(){console.log(3);},0);//enters queue second but its brouhgt to the front of queue due to smaller timer=>Third to be executed
+setTimeout(function(){console.log(3);},999);//enters queue second but its brouhgt to the front of queue due to smaller timer=>Third to be executed
 console.log(4)//Second to be executed
